dff3c44923fcb9a12063723f17b4786924c7c312
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-04-05 19:52:38 +0200

    Added empty implementation of BridgesTester

---
+++ de.feu.ps.bridges.shared/src/main/java/de/feu/ps/bridges/shared/BridgesTesterImpl.java
@@ -0,0 +1,17 @@
+package de.feu.ps.bridges.shared;
+
+/**
+ * Created by tim on 05.04.16.
+ */
+public class BridgesTesterImpl implements BridgesTester {
+
+    @Override
+    public void testGeneratePuzzle(String filePath, int width, int height, int isles) {
+
+    }
+
+    @Override
+    public void testSolvePuzzle(String puzzlePath, String solutionPath) {
+
+    }
+}
