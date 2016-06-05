e9fd7ab3f0fbedff7a048b9572e436faca4a037a
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-06-05 19:54:53 +0200

    JavaDoc for Bridge.java

--- de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/Bridge.java
+++ de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/Bridge.java
@@ -1,10 +1,28 @@
 package de.feu.ps.bridges.model;
 
 /**
+ * Interface for bridges that connect two {@link Island}s.
+ * A bridge can connect two islands two times.
+ *
  * @author Tim Gremplewski
  */
 public interface Bridge {
+
+    /**
+     * The {@link Island} that is connected to the {@link Island} returned by {@link #getIsland2()}.
+     * @return An {@link Island}
+     */
     Island getIsland1();
+
+    /**
+     * The {@link Island} that is connected to the {@link Island} returned by {@link #getIsland1()}.
+     * @return An {@link Island}
+     */
     Island getIsland2();
+
+    /**
+     * Indicates whether this {@link Bridge} connects its {@link Island}s two times.
+     * @return <code>true</code> if this {@link Bridge} connects its {@link Island}s two times.
+     */
     boolean isDoubleBridge();
 }
