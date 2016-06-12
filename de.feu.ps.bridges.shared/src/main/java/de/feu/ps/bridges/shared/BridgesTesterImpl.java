1a12b730b25f0d142bbb739754a106af81d829a5
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-06-12 14:14:02 +0200

    Implemented testGeneratePuzzle

--- de.feu.ps.bridges.shared/src/main/java/de/feu/ps/bridges/shared/BridgesTesterImpl.java
+++ de.feu.ps.bridges.shared/src/main/java/de/feu/ps/bridges/shared/BridgesTesterImpl.java
@@ -1,13 +1,32 @@
 package de.feu.ps.bridges.shared;
 
+import de.feu.ps.bridges.generator.PuzzleGeneratorImpl;
+import de.feu.ps.bridges.model.Puzzle;
+import de.feu.ps.bridges.serialization.Serializer;
+
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * Created by tim on 05.04.16.
+ * @author Tim Gremplewski
  */
 public class BridgesTesterImpl implements BridgesTester {
+
+    private static final Logger logger = Logger.getLogger(BridgesTesterImpl.class.getName());
 
     @Override
     public void testGeneratePuzzle(String filePath, int width, int height, int isles) {
 
+        // TODO Test
+
+        Puzzle puzzle = PuzzleGeneratorImpl.generatePuzzle(width, height, isles);
+        Serializer serializer = new Serializer();
+        try {
+            serializer.storePuzzle(puzzle, filePath);
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "Could not store puzzle.", e);
+        }
     }
 
     @Override
