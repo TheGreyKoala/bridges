ecb44a162f3feba0f9a67526664274c95d2b75fb
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-06-19 19:06:21 +0200

    ModifiablePuzzle should not be public

--- de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/ModifiablePuzzle.java
+++ de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/ModifiablePuzzle.java
@@ -3,6 +3,6 @@ package de.feu.ps.bridges.model;
 /**
  * @author Tim Gremplewski
  */
-public interface ModifiablePuzzle extends Puzzle {
+interface ModifiablePuzzle extends Puzzle {
     void addIsland(Island island);
 }
