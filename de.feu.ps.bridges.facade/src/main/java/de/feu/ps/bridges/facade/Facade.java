e99fdaac1a28d6c3b749a7dce2a2c847fb29fece
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-06-25 19:55:32 +0200

    Use the Facade in the module shared

--- de.feu.ps.bridges.facade/src/main/java/de/feu/ps/bridges/facade/Facade.java
+++ de.feu.ps.bridges.facade/src/main/java/de/feu/ps/bridges/facade/Facade.java
@@ -2,15 +2,19 @@ package de.feu.ps.bridges.facade;
 
 import de.feu.ps.bridges.model.Puzzle;
 import de.feu.ps.bridges.serialization.Deserializer;
+import de.feu.ps.bridges.serialization.Serializer;
+import de.feu.ps.bridges.solver.DefaultSolver;
+import de.feu.ps.bridges.solver.Solver;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * @author Tim Gremplewski
  */
 public class Facade {
 
-    public static Puzzle loadPuzzle(File sourceFile) {
+    public static Puzzle loadPuzzle(final File sourceFile) {
         Deserializer deserializer = new Deserializer();
         try {
             return deserializer.loadPuzzle(sourceFile.getPath());
@@ -19,5 +23,19 @@ public class Facade {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static void storePuzzle(final Puzzle puzzle, final File destinationFile) {
+        Serializer serializer = new Serializer();
+        try {
+            serializer.storePuzzle(puzzle, destinationFile.getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void solvePuzzle(final Puzzle puzzle) {
+        Solver solver = DefaultSolver.createSolverFor(puzzle);
+        solver.solve();
     }
 }
--- de.feu.ps.bridges.shared/src/main/java/de/feu/ps/bridges/shared/BridgesTesterImpl.java
+++ de.feu.ps.bridges.shared/src/main/java/de/feu/ps/bridges/shared/BridgesTesterImpl.java
@@ -1,12 +1,10 @@
 package de.feu.ps.bridges.shared;
 
+import de.feu.ps.bridges.facade.Facade;
 import de.feu.ps.bridges.generator.PuzzleGeneratorImpl;
 import de.feu.ps.bridges.model.Puzzle;
-import de.feu.ps.bridges.serialization.Deserializer;
-import de.feu.ps.bridges.serialization.Serializer;
-import de.feu.ps.bridges.solver.DefaultSolver;
-import de.feu.ps.bridges.solver.Solver;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,8 +27,7 @@ public class BridgesTesterImpl implements BridgesTester {
 
     private void generatePuzzle(String filePath, int width, int height, int isles) throws FileNotFoundException {
         Puzzle puzzle = PuzzleGeneratorImpl.generatePuzzle(width, height, isles);
-        Serializer serializer = new Serializer();
-        serializer.storePuzzle(puzzle, filePath);
+        Facade.storePuzzle(puzzle, new File(filePath));
     }
 
     @Override
@@ -43,11 +40,8 @@ public class BridgesTesterImpl implements BridgesTester {
     }
 
     private void solvePuzzle(String puzzlePath, String solutionPath) throws Exception {
-        Deserializer deserializer = new Deserializer();
-        Puzzle puzzle = deserializer.loadPuzzle(puzzlePath);
-        Solver solver = DefaultSolver.createSolverFor(puzzle);
-        solver.solve();
-        Serializer serializer = new Serializer();
-        serializer.storePuzzle(puzzle, solutionPath);
+        Puzzle puzzle = Facade.loadPuzzle(new File(puzzlePath));
+        Facade.solvePuzzle(puzzle);
+        Facade.storePuzzle(puzzle, new File(solutionPath));
     }
 }
