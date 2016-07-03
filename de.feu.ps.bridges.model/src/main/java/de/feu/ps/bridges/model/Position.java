1e6c3f03c58395f8f3ca90da84937aa10a1c8d14
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-07-03 13:20:15 +0200

    JavaDoc for Position

--- de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/Position.java
+++ de.feu.ps.bridges.model/src/main/java/de/feu/ps/bridges/model/Position.java
@@ -1,12 +1,19 @@
 package de.feu.ps.bridges.model;
 
 /**
+ * Position in the puzzle.
  * @author Tim Gremplewski
  */
 public class Position {
     private final int column;
     private final int row;
 
+    /**
+     * Creates a new instance.
+     * @param column the column of the position
+     * @param row thr row of the position.
+     * @throws IllegalArgumentException if <code>column</code> or <row>row</row> is null.
+     */
     public Position(int column, int row) {
         if (column < 0) {
             throw new IllegalArgumentException("Parameter 'column' must not be less than 0.");
@@ -20,10 +27,18 @@ public class Position {
         this.row = row;
     }
 
+    /**
+     * Get the column of this position.
+     * @return the column of this position.
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * Get the row of this position.
+     * @return the row of this position.
+     */
     public int getRow() {
         return row;
     }
