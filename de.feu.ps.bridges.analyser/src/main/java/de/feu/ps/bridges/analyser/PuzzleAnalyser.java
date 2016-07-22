940477a86be3db0ebdca1cd9c82996f5337f87ee
Tim Gremplewski <tim.gremplewski@gmail.com>
2016-07-22 20:05:29 +0200

    Added missing JavaDoc

--- de.feu.ps.bridges.analyser/src/main/java/de/feu/ps/bridges/analyser/PuzzleAnalyser.java
+++ de.feu.ps.bridges.analyser/src/main/java/de/feu/ps/bridges/analyser/PuzzleAnalyser.java
@@ -20,6 +20,7 @@ public interface PuzzleAnalyser {
     /**
      * Get all valid destinations for a bridge starting at the given island.
      * @param island Island to start from.
+     * @param doubleBridge indicates whether destinations for a single or a double bridge should be returned.
      * @return all valid destinations for a bridge starting at the given island.
      * @throws NullPointerException if island is null.
      */
@@ -29,10 +30,19 @@ public interface PuzzleAnalyser {
      * Indicates whether a bridge in the given direction and starting at the given island would be valid.
      * @param island Island to start the bridge at.
      * @param direction Direction to where to build the bridge.
+     * @param doubleBridge Indicates whether the move should add a single or a double bridge.
      * @return true, if the bridge would be valid, false otherwise.
      * @throws NullPointerException if island or direction is null.
      */
     boolean isValidMove(Island island, Direction direction, boolean doubleBridge);
 
+    /**
+     * Indicates whether a bridge between the given islands would be valid.
+     * @param island1 Island to start the bridge at.
+     * @param island2 Island to stop the bridge at.
+     * @param doubleBridge Indicates whether the move should add a single or a double bridge.
+     * @return true, if the bridge would be valid, false otherwise.
+     * @throws NullPointerException if island1 or islands is null.
+     */
     boolean isValidMove(Island island1, Island island2, boolean doubleBridge);
 }
